44010b243d0c3bc598ae895d9410d6ffd12ab954
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2020-12-04 17:36:13 -0600

    Updates structure to match the official repo's example
    Encapsulates everything under a class

--- testBot.py
+++ testBot.py
@@ -1,48 +1,52 @@
 import discord
 import json
 
-client = discord.Client()
-
 configJsonFile = open('config.json')
 jsonData = json.load(configJsonFile)
 prefix = jsonData['prefix'] #Reference to the prefix stated in the config.json
 intents = discord.Intents(voice_states = True)
 
-#Prints to console when ready
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    lowerCaseMessage = message.content.lower()
-    if lowerCaseMessage == (prefix + "hello") or lowerCaseMessage == (prefix + "hi"):
-        await message.channel.send("Hellou!")
-    elif lowerCaseMessage == (prefix + "usersinchannel"):
-        channel = client.get_channel(273224026464452608)
-        members = channel.members
-        memnicks = []
+class MyClient(discord.Client):
 
-        for member in members:
-            memnicks.append(member.name)
+    #Prints to console when ready
+    async def on_ready(self):
+        print('We have logged in as ' + self.user.name + " " + str(self.user.id))
 
-        await message.channel.send(memnicks)
+    async def on_message(self, message):
+        #Prevents bot from replying to itself
+        if message.author.id == self.user.id:
+            return
 
-def getCurrentUsersInVoiceChannel()
+        #Greeting message
+        lowerCaseMessage = message.content.lower() #Transforms message to lowrcase
+        if lowerCaseMessage == (prefix + "hello") or lowerCaseMessage == (prefix + "hi"): #Compares the lowercase version of the message
+            await message.channel.send("Hello " + message.author.name)
 
-@client.event
-async def on_voice_state_update(member, before, after):
+        #Prints users in General text channel
+        elif lowerCaseMessage == (prefix + "usersinchannel"):
+            channel = client.get_channel(273224026464452608)
+            members = channel.members
+            memnicks = []
 
-    #global membersInChannelCounter
+            for member in members:
+                memnicks.append(member.name)
 
-    if before.channel is None and after.channel is not None:
-        print(member.name + " joined voice channel")
-        #membersInChannelCounter = membersInChannelCounter+1
-        #print("Users in voice channels: " + str(membersInChannelCounter))
+            await message.channel.send(memnicks)
 
-    if before.channel is not None and after.channel is None:
-        print(member.name + " disconnected from voice channel")
-        #membersInChannelCounter = membersInChannelCounter+1
-        #print("Users in voice channels: " + str(membersInChannelCounter))
+    async def on_voice_state_update(self, member, before, after):
 
+        #global membersInChannelCounter
+
+        if before.channel is None and after.channel is not None:
+            print(member.name + " joined voice channel")
+            #membersInChannelCounter = membersInChannelCounter+1
+            #print("Users in voice channels: " + str(membersInChannelCounter))
+
+        if before.channel is not None and after.channel is None:
+            print(member.name + " disconnected from voice channel")
+            #membersInChannelCounter = membersInChannelCounter+1
+            #print("Users in voice channels: " + str(membersInChannelCounter))
+
+client = MyClient()
 client.run(jsonData['token']) #Calls the key from the config.json file
