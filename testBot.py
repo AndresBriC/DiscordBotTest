0e3bc990a97a84111c11964482a621384987a072
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2020-12-14 23:49:33 -0600

    Allows LastToLoeaveLeaderBoard to update
    -Restructures whole structure to allow for @bot.commands usage
    -Adds a way to add new users to LastTolLeaveLeaderBoard
    -Adds a way to update user's score in LastTolLeaveLeaderBoard

--- testBot.py
+++ testBot.py
@@ -1,91 +1,122 @@
 import discord
+from discord.ext import commands
 import json
+import pandas
 
-configJsonFile = open('config.json')
+configJsonFile = open('config.json') #config.json contains the token and prefix
 jsonData = json.load(configJsonFile)
 prefix = jsonData['prefix'] #Reference to the prefix stated in the config.json
+token = jsonData['token']
 intents = discord.Intents(voice_states = True, members = True) #Specifies intents
+client = commands.Bot(command_prefix = prefix)
 
 #Ideas:
 #Contador de días desde ultima salida a San Mike, Alexa play Despacito con comando de voz
 
 inVoiceChannels = 0 #Counts how many people are currently in any voice channel
 
-class MyClient(discord.Client):
+#Returns a bool depending if the indicated user exists within the given pandas database
+def userExists(user, df):
+    found = df[df['User'].str.contains(user)]
 
-    #Returns True if member has "Bots" as a role
-    async def memberIsBot(self, member):
-        isABot = False
+    if len(found) == 0:
+        return False
+    else:
+        return True
 
-        for roles in member.roles: #Loops for each role the member has
-            if roles == "Bots": #If the member has a role named "Bots"
-                print("Member is a bot")
-                isABot = True
-                break
+#Returns the index of the indicated user, within the given database
+def getIndexOfUser(user, df):
+    dataTop = df.head()
+
+    for i in dataTop.index:
+        if df.loc[i]['User'] == user:
+            return i
+
+#Returns True if member has "Bots" as a role
+async def memberIsBot(self, member):
+    isABot = False
+
+    #Checks if the member has the Bots role
+    for role in member.roles:
+        if str(role) == "Bots":
+            print("A bot joined a voice channel")
+            isABot = True
+            break
+    
+    return isABot
+
+#-------------------------------COMMANDS-------------------------------#
+
+@client.command()
+async def hegay(ctx):
+    await ctx.send("He gay")
+
+#Greets the person that calls the command
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello " + ctx.author.name)
+
+#----------------------------------------------------------------------#
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    generalTextChannel = discord.utils.get(member.guild.channels, name = 'general') #Looks for text channel named general
+    leaderboardDf = pandas.read_csv('LastToLeaveLeaderboard.csv', index_col=0) #Used to keep track of last people to leave
+
+    global inVoiceChannels #Counts how many people are currently in any voice channel
+
+    #When the user joins a voice channel
+    if before.channel is None and after.channel is not None:
+        isBot = False
+
+        #Checks if the member has the Bots role
+        for role in member.roles:
+            if str(role) == "Bots":
+                print("A bot joined a voice channel")
+                isBot = True
         
-        return isABot
-
-    #Prints to console when ready
-    async def on_ready(self):
-        print('We have logged in as ' + self.user.name + " ID: " + str(self.user.id))
-
-    async def on_message(self, message):
-        #Prevents bot from replying to itself
-        if message.author.id == self.user.id:
-            return
-
-        #Greeting message
-        lowerCaseMessage = message.content.lower() #Transforms message to lowrcase
-        if lowerCaseMessage == (prefix + "hello") or lowerCaseMessage == (prefix + "hi"): #Compares the lowercase version of the message
-            await message.channel.send("Hello " + message.author.name)
-
-        if lowerCaseMessage == (prefix + "hegay?"):
-            await message.channel.send("He gay ")
-
-
-    async def on_voice_state_update(self, member, before, after):
-        generalTextChannel = discord.utils.get(member.guild.channels, name = 'general') #Looks for text channel named general
-
-        global inVoiceChannels #Counts how many people are currently in any voice channel
-
-        #When the user joins a voice channel
-        if before.channel is None and after.channel is not None:
-            isBot = False
-
-            #Checks if the member has the Bots role
-            for role in member.roles:
-                if str(role) == "Bots":
-                    print("A bot joined a voice channel")
-                    isBot = True
+        #If the member is not a bot
+        if(isBot == False):
+            inVoiceChannels += 1
             
-            #If the member is not a bot
-            if(isBot == False):
-                inVoiceChannels += 1
-                
-            print("People in voice channels: " + str(inVoiceChannels))
-            print(member.name + " connected to " + after.channel.name)
+        print("People in voice channels: " + str(inVoiceChannels))
+        print(member.name + " connected to " + after.channel.name)
 
-        #When a user leaves the voice channels
-        if before.channel is not None and after.channel is None:
-            isBot = False
+    #When a user leaves the voice channels
+    if before.channel is not None and after.channel is None:
+        isBot = False
 
-            #Checks if the member has the Bots role
-            for role in member.roles:
-                if str(role) == "Bots":
-                    print("A bot left a voice channel")
-                    isBot = True
-            
-            #If the member is not a bot
-            if(isBot == False):
-                inVoiceChannels -= 1
+        #Checks if the member has the Bots role
+        for role in member.roles:
+            if str(role) == "Bots":
+                print("A bot left a voice channel")
+                isBot = True
+        
+        #If the member is not a bot
+        if(isBot == False and inVoiceChannels > 0):
+            inVoiceChannels -= 1
 
-            print("People in voice channels: " + str(inVoiceChannels))
-            print(member.name + " disconnected from " + before.channel.name)
-            
-            #If the number of people in the voice channel is 0
-            if(inVoiceChannels == 0 and isBot == False):
-                print(member.name + " is gay lol")
-                await generalTextChannel.send(member.name + " is gay af :eggplant:")
+        print("People in voice channels: " + str(inVoiceChannels))
+        print(member.name + " disconnected from " + before.channel.name)
+        
+        #If the number of people in the voice channel is 0 and the user that left is not a bot
+        if(inVoiceChannels == 0 and isBot == False):
+            print(member.name + " is gay lol")
+            await generalTextChannel.send(member.name)
 
-client = MyClient()
-client.run(jsonData['token']) #Calls the key from the config.json file
+            #Adds user to the leaderboard if not present, if not, adds 1 point to the user
+            if userExists(member.name, leaderboardDf) == True:
+                index = getIndexOfUser(member.name, leaderboardDf)
+                leaderboardDf.at[index, 'Score'] = leaderboardDf.loc[index]['Score'] + 1
+                print("User was found")
+            else:
+                leaderboardDf = leaderboardDf.append({'User':member.name,'Score':1}, ignore_index=True)
+                print("User was added")
+
+            leaderboardDf.to_csv('LastToLeaveLeaderboard.csv')
+
+client.run(token) #Calls the key from the config.json file
